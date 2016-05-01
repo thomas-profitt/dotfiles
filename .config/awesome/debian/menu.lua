d7ba7be27c61b3f828ac0dc30100e138f60eb163
Thomas Profitt <tomdprofitt@gmail.com>
2016-05-01 13:46:05 -0500

    Add awesome wm config

---
+++ .config/awesome/debian/menu.lua
@@ -0,0 +1,170 @@
+-- automatically generated file. Do not edit (see /usr/share/doc/menu/html)
+
+module("debian.menu")
+
+Debian_menu = {}
+
+Debian_menu["Debian_Applications_Accessibility"] = {
+	{"Xmag","xmag"},
+}
+Debian_menu["Debian_Applications_Data_Management"] = {
+	{"LibreOffice Base","/usr/bin/libreoffice --base","/usr/share/icons/hicolor/32x32/apps/libreoffice-base.xpm"},
+}
+Debian_menu["Debian_Applications_Editors"] = {
+	{"GVIM","/usr/bin/vim.gtk -g -f","/usr/share/pixmaps/vim-32.xpm"},
+	{"Nano", "x-terminal-emulator -e ".."/bin/nano","/usr/share/nano/nano-menu.xpm"},
+	{"Xedit","xedit"},
+}
+Debian_menu["Debian_Applications_Emulators"] = {
+	{"PlayOnLinux","/usr/bin/playonlinux","/usr/share/pixmaps/playonlinux.xpm"},
+	{"VirtualBox","/usr/bin/virtualbox","/usr/share/pixmaps/virtualbox.xpm"},
+}
+Debian_menu["Debian_Applications_File_Management"] = {
+	{"Baobab","/usr/bin/baobab","/usr/share/pixmaps/baobab.xpm"},
+	{"Thunar","/usr/bin/thunar"},
+	{"Xfdesktop","xfdesktop"},
+}
+Debian_menu["Debian_Applications_Graphics"] = {
+	{"gcolor2","/usr/bin/gcolor2","/usr/share/pixmaps/gcolor2/gcolor2.xpm"},
+	{"LibreOffice Draw","/usr/bin/libreoffice --draw","/usr/share/icons/hicolor/32x32/apps/libreoffice-draw.xpm"},
+	{"The GIMP","/usr/bin/gimp","/usr/share/pixmaps/gimp.xpm"},
+	{"X Window Snapshot","xwd | xwud"},
+}
+Debian_menu["Debian_Applications_Network_Communication"] = {
+	{"Telnet", "x-terminal-emulator -e ".."/usr/bin/telnet.netkit"},
+	{"Xbiff","xbiff"},
+	{"X Chat","/usr/bin/xchat","/usr/share/icons/xchat.xpm"},
+}
+Debian_menu["Debian_Applications_Network_File_Transfer"] = {
+	{"Transmission BitTorrent Client (GTK)","/usr/bin/transmission-gtk","/usr/share/pixmaps/transmission.xpm"},
+}
+Debian_menu["Debian_Applications_Network_Web_Browsing"] = {
+	{"xombrero","/usr/bin/xombrero","/usr/share/pixmaps/xombrero.xpm"},
+}
+Debian_menu["Debian_Applications_Network"] = {
+	{ "Communication", Debian_menu["Debian_Applications_Network_Communication"] },
+	{ "File Transfer", Debian_menu["Debian_Applications_Network_File_Transfer"] },
+	{ "Web Browsing", Debian_menu["Debian_Applications_Network_Web_Browsing"] },
+}
+Debian_menu["Debian_Applications_Office"] = {
+	{"LibreOffice Calc","/usr/bin/libreoffice --calc","/usr/share/icons/hicolor/32x32/apps/libreoffice-calc.xpm"},
+	{"LibreOffice Impress","/usr/bin/libreoffice --impress","/usr/share/icons/hicolor/32x32/apps/libreoffice-impress.xpm"},
+	{"LibreOffice Writer","/usr/bin/libreoffice --writer","/usr/share/icons/hicolor/32x32/apps/libreoffice-writer.xpm"},
+}
+Debian_menu["Debian_Applications_Programming"] = {
+	{"GDB", "x-terminal-emulator -e ".."/usr/bin/gdb"},
+	{"Meld","/usr/bin/meld"},
+	{"Python (v2.7)", "x-terminal-emulator -e ".."/usr/bin/python2.7","/usr/share/pixmaps/python2.7.xpm"},
+	{"Python (v3.4)", "x-terminal-emulator -e ".."/usr/bin/python3.4","/usr/share/pixmaps/python3.4.xpm"},
+	{"Tclsh8.6", "x-terminal-emulator -e ".."/usr/bin/tclsh8.6"},
+	{"TkWish8.6","x-terminal-emulator -e /usr/bin/wish8.6"},
+}
+Debian_menu["Debian_Applications_Science_Mathematics"] = {
+	{"Bc", "x-terminal-emulator -e ".."/usr/bin/bc"},
+	{"Dc", "x-terminal-emulator -e ".."/usr/bin/dc"},
+	{"LibreOffice Math","/usr/bin/libreoffice --math","/usr/share/icons/hicolor/32x32/apps/libreoffice-math.xpm"},
+	{"Xcalc","xcalc"},
+}
+Debian_menu["Debian_Applications_Science"] = {
+	{ "Mathematics", Debian_menu["Debian_Applications_Science_Mathematics"] },
+}
+Debian_menu["Debian_Applications_Shells"] = {
+	{"Bash", "x-terminal-emulator -e ".."/bin/bash --login"},
+	{"Dash", "x-terminal-emulator -e ".."/bin/dash -i"},
+	{"Sh", "x-terminal-emulator -e ".."/bin/sh --login"},
+	{"Zsh", "x-terminal-emulator -e ".."/bin/zsh5"},
+}
+Debian_menu["Debian_Applications_Sound"] = {
+	{"pavucontrol","/usr/bin/pavucontrol"},
+}
+Debian_menu["Debian_Applications_System_Administration"] = {
+	{"DSL/PPPoE configuration tool", "x-terminal-emulator -e ".."/usr/sbin/pppoeconf","/usr/share/pixmaps/pppoeconf.xpm"},
+	{"Editres","editres"},
+	{"GNOME partition editor","su-to-root -X -c /usr/sbin/gparted","/usr/share/pixmaps/gparted.xpm"},
+	{"menulibre","menulibre"},
+	{"mugshot","/usr/bin/mugshot"},
+	{"Network Admin","/usr/bin/network-admin","/usr/share/gnome-system-tools/pixmaps/network.xpm"},
+	{"pppconfig", "x-terminal-emulator -e ".."su-to-root -p root -c /usr/sbin/pppconfig"},
+	{"Services Admin","/usr/bin/services-admin","/usr/share/gnome-system-tools/pixmaps/services.xpm"},
+	{"Shares Admin","/usr/bin/shares-admin","/usr/share/gnome-system-tools/pixmaps/shares.xpm"},
+	{"Time Admin","/usr/bin/time-admin","/usr/share/gnome-system-tools/pixmaps/time.xpm"},
+	{"User accounts Admin","/usr/bin/users-admin","/usr/share/gnome-system-tools/pixmaps/users.xpm"},
+	{"Xclipboard","xclipboard"},
+	{"Xfce Application Finder","xfce4-appfinder","/usr/share/pixmaps/xfce4-appfinder.xpm"},
+	{"Xfontsel","xfontsel"},
+	{"Xkill","xkill"},
+	{"Xrefresh","xrefresh"},
+}
+Debian_menu["Debian_Applications_System_Hardware"] = {
+	{"Xvidtune","xvidtune"},
+}
+Debian_menu["Debian_Applications_System_Language_Environment"] = {
+	{"Input Method Configuration", "x-terminal-emulator -e ".."/usr/bin/im-config"},
+}
+Debian_menu["Debian_Applications_System_Monitoring"] = {
+	{"GNOME system monitor","/usr/bin/gnome-system-monitor"},
+	{"htop", "x-terminal-emulator -e ".."/usr/bin/htop"},
+	{"Pstree", "x-terminal-emulator -e ".."/usr/bin/pstree.x11","/usr/share/pixmaps/pstree16.xpm"},
+	{"Top", "x-terminal-emulator -e ".."/usr/bin/top"},
+	{"Xconsole","xconsole -file /dev/xconsole"},
+	{"Xev","x-terminal-emulator -e xev"},
+	{"Xload","xload"},
+}
+Debian_menu["Debian_Applications_System_Package_Management"] = {
+	{"Synaptic Package Manager","x-terminal-emulator -e synaptic-pkexec","/usr/share/synaptic/pixmaps/synaptic_32x32.xpm"},
+}
+Debian_menu["Debian_Applications_System"] = {
+	{ "Administration", Debian_menu["Debian_Applications_System_Administration"] },
+	{ "Hardware", Debian_menu["Debian_Applications_System_Hardware"] },
+	{ "Language Environment", Debian_menu["Debian_Applications_System_Language_Environment"] },
+	{ "Monitoring", Debian_menu["Debian_Applications_System_Monitoring"] },
+	{ "Package Management", Debian_menu["Debian_Applications_System_Package_Management"] },
+}
+Debian_menu["Debian_Applications_Terminal_Emulators"] = {
+	{"Gnome Terminal","/usr/bin/gnome-terminal","/usr/share/pixmaps/gnome-terminal.xpm"},
+}
+Debian_menu["Debian_Applications_Video"] = {
+	{"VLC media player","/usr/bin/qvlc","/usr/share/icons/hicolor/32x32/apps/vlc.xpm"},
+}
+Debian_menu["Debian_Applications_Viewers"] = {
+	{"Evince","/usr/bin/evince","/usr/share/pixmaps/evince.xpm"},
+	{"Xditview","xditview"},
+}
+Debian_menu["Debian_Applications"] = {
+	{ "Accessibility", Debian_menu["Debian_Applications_Accessibility"] },
+	{ "Data Management", Debian_menu["Debian_Applications_Data_Management"] },
+	{ "Editors", Debian_menu["Debian_Applications_Editors"] },
+	{ "Emulators", Debian_menu["Debian_Applications_Emulators"] },
+	{ "File Management", Debian_menu["Debian_Applications_File_Management"] },
+	{ "Graphics", Debian_menu["Debian_Applications_Graphics"] },
+	{ "Network", Debian_menu["Debian_Applications_Network"] },
+	{ "Office", Debian_menu["Debian_Applications_Office"] },
+	{ "Programming", Debian_menu["Debian_Applications_Programming"] },
+	{ "Science", Debian_menu["Debian_Applications_Science"] },
+	{ "Shells", Debian_menu["Debian_Applications_Shells"] },
+	{ "Sound", Debian_menu["Debian_Applications_Sound"] },
+	{ "System", Debian_menu["Debian_Applications_System"] },
+	{ "Terminal Emulators", Debian_menu["Debian_Applications_Terminal_Emulators"] },
+	{ "Video", Debian_menu["Debian_Applications_Video"] },
+	{ "Viewers", Debian_menu["Debian_Applications_Viewers"] },
+}
+Debian_menu["Debian_Games_Toys"] = {
+	{"Oclock","oclock"},
+	{"Xclock (analog)","xclock -analog"},
+	{"Xclock (digital)","xclock -digital -update 1"},
+	{"Xeyes","xeyes"},
+	{"Xlogo","xlogo"},
+}
+Debian_menu["Debian_Games"] = {
+	{ "Toys", Debian_menu["Debian_Games_Toys"] },
+}
+Debian_menu["Debian_Help"] = {
+	{"Info", "x-terminal-emulator -e ".."info"},
+	{"Xman","xman"},
+	{"yelp","/usr/bin/yelp"},
+}
+Debian_menu["Debian"] = {
+	{ "Applications", Debian_menu["Debian_Applications"] },
+	{ "Games", Debian_menu["Debian_Games"] },
+	{ "Help", Debian_menu["Debian_Help"] },
+}
